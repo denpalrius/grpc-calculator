3d2ab4df4a14788bdf7bd96e6eec3e5ad88dd1ba
Dennis Riungu <2971971+denpalrius@users.noreply.github.com>
2024-09-21 19:15:16 -0500

    Added client

--- server.py
+++ server.py
@@ -1,26 +1,28 @@
 from concurrent import futures
+import os
 import grpc
 from square_pb2 import Result
 import square_pb2_grpc
-import math
 
 
 class SquareService(square_pb2_grpc.SquareServiceServicer):
-    def Square(self, request, context):
-        square = math.pow(request.value, 2)
+    def GetSquare(self, request, context):
+        square =request.value ** 2
+        print(f"SquareService: Received request for {request.value}. Returning {square}.")
         return Result(value=square)
-    
+
 
 def serve():
-    print("Starting server...")
+    print("SquareServer: Starting...")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     square_pb2_grpc.add_SquareServiceServicer_to_server(SquareService(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port("[::]:50051")
     server.start()
 
-    print("Server started at 50051.")
+    print("SquareServer: Server started at 50051.")
     server.wait_for_termination()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    os.system('cls' if os.name == 'nt' else 'clear')
     serve()
